cdc5b7b2ab425d4909ad4bc03186a4ad896c6bd4
Dhruv_0007 <dwivedid544@gmail.com>
2026-09-20 18:00:53 +0530

    fix(app): remove app.options('*') incompatible with Express 5 path-to-regexp

--- src/app.js
+++ src/app.js
@@ -66,7 +66,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
